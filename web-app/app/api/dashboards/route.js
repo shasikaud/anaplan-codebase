2e4f2bde1b208ab1e8146ce430af286bd12b7f5e
Shasika Udayanga <shasikaudayanga@gmail.com>
2024-06-11 18:05:05 +0100

    removed POST

--- web-app/app/api/dashboards/route.js
+++ web-app/app/api/dashboards/route.js
@@ -10,14 +10,3 @@ export const GET = async (req) => {
         return NextResponse.json({ error }, { status: 500 });
     }
 };
-
-export const POST = async (req) => {
-    const data = await db.dashboards.create({
-        data: {
-            title: 'New Dashboard'
-        }
-    })
-    return NextResponse.json({
-        data
-    });
-}
